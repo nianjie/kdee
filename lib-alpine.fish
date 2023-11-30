a5b4c89a240d6614127b4d05eb4bf9d926f21220
nj <4769034+nianjie@users.noreply.github.com>
2023-11-30 19:51:27 +0900

    implement configure worker function for alpine.

--- lib-alpine.fish
+++ lib-alpine.fish
@@ -20,7 +20,7 @@ function configure_cgroup
   begin
     set -l cgroup_path /sys/fs/cgroup
     set -l processes (incus exec $container_name -- cat $cgroup_path/cgroup.procs )
-    test -z "$processes" ; and exit_error "Faild to configure control-plane node." 1
+    test -z "$processes" ; and exit_error "Faild to configure $container_name." 1
     incus exec $container_name -- mkdir -p $cgroup_path/init.scope
     and for p in $processes
       incus exec $container_name -- sh -c "echo $p > $cgroup_path/init.scope/cgroup.procs"
@@ -39,14 +39,18 @@ end
 function configure_worker_impl
   set -l cluster_name $argv[1]
   set -l container_name $argv[2]
-  begin
-    incus config device add $container_name k3s-binary disk source=$kubdee_dir/clusters/$cluster_name/rootfs/usr/local/bin/k3s path=/usr/local/bin/k3s
-  end &> /dev/null
   set -l controller_name kubdee-$cluster_name-controller
   set -l token (incus exec $controller_name -- cat /var/lib/rancher/k3s/server/node-token)
   or exit_error "k3s server token not found on $controller_name. Dose the server run?" 1
   set -l server_ip4 (container_ipv4_address $controller_name)
   begin
-      incus exec $container_name -- k3s agent --server https://$server_ip4:6443 --token $token &
-  end &>/dev/null
+    configure_cgroup $container_name
+    incus exec $container_name -- sh -c "echo '# k3s options
+export PATH=\"/usr/libexec/cni/;\$PATH\"
+K3S_EXEC=\"agent\"
+K3S_OPTS=\"--server https://$server_ip4:6443 --token $token \"
+' > /etc/conf.d/k3s
+"
+    incus exec $container_name -- rc-service k3s start
+  end #&>/dev/null
 end
